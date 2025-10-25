af42e9e02360e4e5c51f76040ae23f9335beb59f
guinto <guintoramachilles@gmail.com>
2025-10-25 12:38:36 +0800

    Added more stuff

--- index.js
+++ index.js
@@ -22,3 +22,13 @@ decrementButtons.forEach(button => {
         }
     });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
