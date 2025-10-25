eb3c55de1a6005487bc5bb3d698b282829ba298c
guinto <guintoramachilles@gmail.com>
2025-10-25 10:46:38 +0800

    Added more stuff

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("Hello World");
-/* This comment is a test *
+const incrementButtons = document.querySelectorAll('.incrementButton');
+const decrementButtons = document.querySelectorAll('.decrementButton');
+
+incrementButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const card = e.currentTarget.closest('.card-items');
+        const counterDisplay = card.querySelector('.quantity-value');
+        let count = parseInt(counterDisplay.textContent);
+        count++;
+        counterDisplay.textContent = count;
+    });
+});
+
+decrementButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const card = e.currentTarget.closest('.card-items');
+        const counterDisplay = card.querySelector('.quantity-value');
+        let count = parseInt(counterDisplay.textContent);
+        if (count > 0) {
+            count--;
+            counterDisplay.textContent = count;
+        }
+    });
+});
